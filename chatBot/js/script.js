087a7f30d7745eb3b6b8f2665f82217b44fcc5e4
PandaBwoy <36724360+cheesypanda1@users.noreply.github.com>
2019-07-16 13:06:53 -0700

    Changes promoted including JSON, AJAX and PHP
    Created JSON files, applied AJAX and write PHP for object creation

--- chatBot/js/script.js
+++ chatBot/js/script.js
@@ -6,18 +6,54 @@ $(document).ready(function(){
     });
 
     //Convo 1
-        $("<div class='botMsg'><span>"+"I am Panda, your name?"+"</span></div>").appendTo(".chatArea");
-
         $("#sbBtn").click(function(){
-            let name = $("#msgInp").val();
-            $("<div class='userMsg'><span>"+name+"</span></div>").appendTo(".chatArea");
-            $("#msgInp").val("");
-            setTimeout(function(){
-            $("<div class='botMsg'><span>"+"Hi "+name+", nice to meet you"+"</span></div>").appendTo(".chatArea");
-            setTimeout(function(){
-                $("<div class='botMsg'><span>"+"How may i help you?"+"</span></div>").appendTo(".chatArea");
-            },500);
-        },500);
+            let userMsg = $("#msgInp").val();
+            let userMsgData = new FormData();
+            userMsgData.append("userMsg",userMsg);
+            addMessage(userMsgData);
+            function addMessage(userMsgData){
+                $.ajax({
+                    type: 'POST',
+                    url:  './php/userMessage.php',
+                    data: userMsgData,
+                    contentType: false,       // The content type used when sending data to the server.
+                    cache: false,             // To unable request pages to be cached
+                    processData:false,
+                    success:function(data)
+                    {
+                        if(data=="ok"){
+                            console.log("message sent to userResponse.json");
+                            $("#msgInp").val("");    
+                        }
+                        else{
+                            console.log(data);
+                        }
+                    }
+                });
+            }
+            // userChatInput = [];
+            // function userMsgArray(){
+            //     userChatInput.push($("#msgInp").val());
+            //     $("#msgInp").val("");
+            // }
+            // userMsgArray();
+            // console.log(userChatInput);
     });
     //Convo 2
+    $.ajax({
+        type: "GET",
+        url: "botResponse.json",
+        dataType: "json",
+        cache: false,             // To unable request pages to be cached
+        processData:false,      
+        contentType: false,     // The content type used when sending data to the server
+        success: function(data) {
+            let Greetings = data[0].greet;
+            let Questions = data[0].question;
+            let randomGreet = Greetings[Math.floor(Math.random()*Greetings.length)];
+            let randonQuestion = Questions[Math.floor(Math.random()*Questions.length)];
+            $("<div class='botMsg'><span>"+randomGreet+"</span></div>").appendTo(".chatArea");
+            //$("<div class='botMsg'><span>"+randonQuestion+"</span></div>").appendTo(".chatArea");
+        }
+    });
 });
